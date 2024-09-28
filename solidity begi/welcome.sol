4420fc7ea1ded4195e7bc7b6965f9be02e78ba36
riyanparag1809 <srikantaprasad2003@gmail.com>
2024-09-28 19:21:54 +0530

    e

--- solidity begi/welcome.sol
+++ solidity begi/welcome.sol
@@ -4,11 +4,27 @@ pragma solidity ^0.8.27;//solidity version
 contract SimpleStorage{
      //intialise favoriteNUmber to 0
      uint256  public favouriteNumber;
+    struct Person{
+      string name;
+      uint256 age;
 
+    }
+    //mapping
+    mapping(string=>uint256) public nameTOAge;
 
+   Person[] public peopleList;
+     
    function store(uint256 _favouriteNumber) public {
     favouriteNumber=_favouriteNumber;
    }
+   function retrieve() public view returns(uint256){
+    return favouriteNumber;
+   }
+   function addPerson(string memory _name,uint256 _age) public {
+      peopleList.push(Person(_name,_age));
+      //mapping code
+      nameTOAge[_name]=_age;
+   }  
    
 
 

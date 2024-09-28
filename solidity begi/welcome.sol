7f790426db6cacc92cd67c5c020852a4d069454b
riyanparag1809 <srikantaprasad2003@gmail.com>
2024-09-28 14:18:38 +0530

    changes

---
+++ solidity begi/welcome.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier :MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;//solidity version
+contract SimpleStorage{
+     //intialise favoriteNUmber to 0
+     uint256  public favouriteNumber;
+
+
+   function store(uint256 _favouriteNumber) public {
+    favouriteNumber=_favouriteNumber;
+   }
+   
+
+
+   
+    
+     
+}
+//simple smart contract for finance

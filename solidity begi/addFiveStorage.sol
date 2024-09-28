55145697675cacb03022c0ef819b6a6469cd144f
riyanparag1809 <srikantaprasad2003@gmail.com>
2024-09-29 01:04:35 +0530

    4

--- solidity begi/addFiveStorage.sol
+++ solidity begi/addFiveStorage.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.24;
-    import {welcome} from "./welcome.sol"
+    import {welcome} from "./welcome.sol";
 
     contract SimpleStorageFactory is welcome {
     
-        function store(uint256 _newNumber) public{
-        number = _newNumber+5;
+        function store(uint256 _newNumber) public override {
+        favouriteNumber= _newNumber+5;
         }
     }

1a608510bb549bea24bf1b59c9d372e10f7fb925
riyanparag1809 <srikantaprasad2003@gmail.com>
2024-09-30 17:08:27 +0530

    funfMe.sol

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,6 +1,21 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.27;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-contract fundMe{
-    
+contract FundMe {
+    uint256 public fundAmounts;
+
+    constructor() payable  {
+        // Initialize fundAmounts to 3
+        fundAmounts = 3;
+    }
+
+    function fundMe() public payable {
+        // Increment fundAmounts by 2
+        uint256 priceOfThat=9;
+         priceOfThat+= 2;
+
+        // Require that the sender sends some Ether
+        require(msg.value >= fundAmounts ,"You must send some Ether");
+    }
 }
